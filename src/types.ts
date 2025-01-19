d54f6f35a5913b1ff7b4dca2f4e7612514e0a452
Ivan Shevchuk <vanosheva99@gmail.com>
2025-01-20 01:12:20 +0200

    final build

--- src/types.ts
+++ src/types.ts
@@ -1,11 +1,10 @@
-import { todayStyle } from './style';
 import {ViewStyle, TextStyle} from 'react-native';
 import {MarkingProps} from './calendar/day/marking';
 import {CalendarContextProps} from './expandableCalendar/Context';
 
 export type ContextProp = {
   context?: CalendarContextProps;
-}
+};
 export type MarkingTypes = 'dot' | 'multi-dot' | 'period' | 'multi-period' | 'custom';
 export type MarkedDates = {
   [key: string]: MarkingProps;
@@ -80,11 +79,11 @@ export interface Theme {
   reservationsBackgroundColor?: string;
   stylesheet?: {
     calendar?: {
-      main?: object; 
+      main?: object;
       header?: object;
     };
     day?: {
-      basic?: object; 
+      basic?: object;
       period?: object;
     };
     dot?: object;
@@ -93,7 +92,7 @@ export interface Theme {
       main?: object;
     };
     agenda?: {
-      main?: object; 
+      main?: object;
       list?: object;
     };
     expandable?: {
@@ -106,11 +105,11 @@ export type AgendaEntry = {
   name: string;
   height: number;
   day: string;
-}
+};
 
 export type AgendaSchedule = {
   [date: string]: AgendaEntry[];
-}
+};
 
 export interface DayAgenda {
   reservation?: AgendaEntry;
